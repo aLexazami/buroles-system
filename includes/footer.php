a63e0e071144f9e04b6735b779d6f2626594d3dc
Alex Flores <alexazamiflores123@gmail.com>
2025-10-03 11:01:05 +0800

    makeas responsive the footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="bg-emerald-950 mt-auto w-full">
   <section class="text-center py-3">
-    <p class="text-white text-sm">
+    <p class="text-white text-xs md:text-sm">
       Copyright &copy; <?= date("Y"); ?>. Burol Elementary School. All rights reserved.
     </p>
   </section>
