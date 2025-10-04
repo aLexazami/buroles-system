681b81435a356b0240a07b30270c96fcba676ce1
Alex Flores <alexazamiflores123@gmail.com>
2025-10-04 09:45:33 +0800

    Make Responsive

--- pages/super-admin/manage-users.php
+++ pages/super-admin/manage-users.php
@@ -17,26 +17,26 @@ renderHead('Super Admin');
   <main class="grid grid-cols-1 md:grid-cols-[248px_1fr] min-h-screen">
     <?php include('../../includes/side-nav-super-admin.php'); ?>
 
-    <section class="m-4">
+    <section class="p-4 sm:p-6 md:p-8">
       <div class="bg-emerald-300 flex justify-center items-center gap-2 p-2 mb-5">
         <img src="/assets/img/manage-user.png " class="w-5 h-5 sm:w-6 sm:h-6">
         <h1 class="font-bold text-lg ">User Management</h1>
       </div>
 
       <!-- Confirmation Modal -->
-      <div id="confirm-modal" class="hidden fixed inset-0 z-50 items-center justify-center">
+      <div id="confirm-modal" class="fixed inset-0 z-50 hidden items-center justify-center px-4 sm:px-0">
         <div class="absolute inset-0 bg-black opacity-50 z-0"></div>
-        <div class="relative bg-white p-6  rounded-4xl shadow-md w-full max-w-md z-10 border border-emerald-500">
-          <p id="confirm-modal-message" class="text-lg mb-10 mt-5 font-semibold text-center">
+         <div class="relative z-10 bg-white p-4 sm:p-6 rounded-2xl shadow-md w-full max-w-sm sm:max-w-md border border-emerald-500">
+          <p id="confirm-modal-message" class="text-lg sm:text-2xl mb-10 mt-5 font-semibold text-center">
             Are you sure you want to proceed?
           </p>
           <div class="flex justify-between gap-4 px-8 mb-5">
             <button id="confirm-modal-yes"
-              class="px-3 py-1 bg-emerald-700 cursor-pointer rounded text-white w-full  hover:bg-emerald-500 focus:outline-none focus:ring-2 focus:ring-green-300 transition">
+              class="text-sm px-3 py-1 bg-emerald-700 cursor-pointer rounded text-white w-full  hover:bg-emerald-500 focus:outline-none focus:ring-2 focus:ring-green-300 transition">
               Yes
             </button>
             <button id="confirm-modal-no"
-              class="px-3 py-1  bg-emerald-700 cursor-pointer rounded text-white w-full hover:bg-emerald-500 focus:outline-none focus:ring-2 focus:ring-red-300 transition">
+              class="text-sm px-3 py-1  bg-emerald-700 cursor-pointer rounded text-white w-full hover:bg-emerald-500 focus:outline-none focus:ring-2 focus:ring-red-300 transition">
               No
             </button>
           </div>
@@ -51,10 +51,10 @@ renderHead('Super Admin');
       </div>
 
       <!-- Super Admin Password Modal -->
-      <div id="passwordModal" class="hidden fixed inset-0  bg-opacity-50 items-center justify-center z-50">
+      <div id="passwordModal" class="fixed inset-0 z-50 hidden items-center justify-center px-4 sm:px-0">
         <div class="absolute inset-0 bg-black opacity-50 z-0"></div>
-        <div class="relative bg-white p-6 rounded-4xl shadow-md w-full max-w-md z-10 border border-emerald-500">
-          <h2 class="text-2xl font-semibold mb-8">Super Admin Verification</h2>
+        <div class="relative z-10 bg-white p-4 sm:p-6 rounded-2xl shadow-md w-full max-w-sm sm:max-w-md border border-emerald-500">
+          <h2 class="text-xl sm:text-2xl font-semibold mb-8">Super Admin Verification</h2>
           <div class="relative">
             <input type="password" id="superAdminPasswordInput" class="w-full border px-3 py-2 rounded mb-4" placeholder="Enter your password">
             <img
@@ -65,8 +65,8 @@ renderHead('Super Admin');
           </div>
           <input type="hidden" id="targetUserId">
           <div class="flex justify-end gap-2">
-            <button id="cancelSuperAdminPassword" class="px-3 py-1 text-emerald-700 rounded hover:bg-emerald-100 cursor-pointer">Cancel</button>
-            <button id="submitSuperAdminPassword" class="px-3 py-1 bg-emerald-700 text-white rounded hover:bg-emerald-500 cursor-pointer">Verify</button>
+            <button id="cancelSuperAdminPassword" class="text-sm px-3 py-1 text-emerald-700 rounded hover:bg-emerald-100 cursor-pointer">Cancel</button>
+            <button id="submitSuperAdminPassword" class="text-sm px-3 py-1 bg-emerald-700 text-white rounded hover:bg-emerald-500 cursor-pointer">Verify</button>
           </div>
         </div>
       </div>
