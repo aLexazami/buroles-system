47e1bce85fad150944f2abdba0e4ea385e93f6c3
Alex Flores <alexazamiflores123@gmail.com>
2025-10-04 09:48:40 +0800

    Makes responsive

--- pages/super-admin/archived-users.php
+++ pages/super-admin/archived-users.php
@@ -26,19 +26,19 @@ renderHead('Super Admin');
       </div>
 
       <!-- Confirmation Modal -->
-      <div id="confirm-modal" class="hidden fixed inset-0 z-50 items-center justify-center">
+      <div id="confirm-modal" class="fixed inset-0 z-50 hidden items-center justify-center px-4 sm:px-0">
         <div class="absolute inset-0 bg-black opacity-50 z-0"></div>
-        <div class="relative bg-white p-6  rounded-4xl shadow-md w-full max-w-md z-10 border border-emerald-500">
-          <p id="confirm-modal-message" class="text-lg mb-10 mt-5 font-semibold text-center">
+         <div class="relative z-10 bg-white p-4 sm:p-6 rounded-2xl shadow-md w-full max-w-sm sm:max-w-md border border-emerald-500">
+          <p id="confirm-modal-message" class="text-lg sm:text-2xl mb-10 mt-5 font-semibold text-center">
             Are you sure you want to proceed?
           </p>
           <div class="flex justify-between gap-4 px-8 mb-5">
             <button id="confirm-modal-yes"
-              class="px-3 py-1 bg-emerald-700 cursor-pointer rounded text-white w-full  hover:bg-emerald-500 focus:outline-none focus:ring-2 focus:ring-green-300 transition">
+              class="text-sm px-3 py-1 bg-emerald-700 cursor-pointer rounded text-white w-full  hover:bg-emerald-500 focus:outline-none focus:ring-2 focus:ring-green-300 transition">
               Yes
             </button>
             <button id="confirm-modal-no"
-              class="px-3 py-1  bg-emerald-700 cursor-pointer rounded text-white w-full hover:bg-emerald-500 focus:outline-none focus:ring-2 focus:ring-red-300 transition">
+              class="text-sm px-3 py-1  bg-emerald-700 cursor-pointer rounded text-white w-full hover:bg-emerald-500 focus:outline-none focus:ring-2 focus:ring-red-300 transition">
               No
             </button>
           </div>
