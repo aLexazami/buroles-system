c43ce99dd297562c87874281276906d192c61278
Alex Flores <alexazamiflores123@gmail.com>
2025-10-15 06:57:19 +0800

    fix fix

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/**/*.css',
+    './pages/**/*.php',
+    './includes/**/*.php',
+    './components/**/*.php',
+    './assets/js/**/*.js',
+    './*.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
